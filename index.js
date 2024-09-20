1bbca325afbf42fd47ee64cdccb1798548486355
Victoria <shevvictoryia@gmail.com>
2024-09-20 12:46:10 +0300

    feat: add data on the page

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Step 1: Make sure that when a user visits the home page,
-//   it shows a random activity.You will need to check the format of the
-//   JSON data from response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
+    console.log(result);
     res.render("index.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
@@ -30,15 +28,15 @@ app.post("/", async (req, res) => {
     const response = await axios.get(
       `https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`
     );
-    const res = response.data;
-    console.log("res", res);
+    const result = response.data;
+    console.log(result);
     res.render("index.ejs", {
-      data: res[Math.floor(Math.random() * res.length)],
+      data: result[Math.floor(Math.random() * result.length)],
     });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
-      error: error.message,
+      error: "No activities that match your criteria.",
     });
   }
 });
