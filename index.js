80b9b6017a22231ae5e6c3cb2f85bed5b9a837ad
Victoria <shevvictoryia@gmail.com>
2024-09-20 12:26:25 +0300

    feat: add post method for activity

--- index.js
+++ index.js
@@ -25,16 +25,22 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
-
-  // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
-  // Render the index.ejs file with a single *random* activity that comes back
-  // from the API request.
-  // Step 3: If you get a 404 error (resource not found) from the API request.
-  // Pass an error to the index.ejs to tell the user:
-  // "No activities that match your criteria."
+  try {
+    console.log(req.body);
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`
+    );
+    const res = response.data;
+    console.log("res", res);
+    res.render("index.ejs", {
+      data: res[Math.floor(Math.random() * res.length)],
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
 app.listen(port, () => {
